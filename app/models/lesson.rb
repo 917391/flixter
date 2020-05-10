24cd7916b59eef73ab00bbb0ab648ea7f65f2b27
917391 <quintonchinouya@gmail.com>
2020-05-10 18:02:47 +0000

    adding the drag and drop functionality for lessons.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,6 @@ class Lesson < ApplicationRecord
   belongs_to :section
   mount_uploader :video, VideoUploader
 
-  include RankedModel
+  include RankedModel 
   ranks :row_order, with_same: :section_id
 end
